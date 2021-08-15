47cd8b35f55dce7aa0dbe04983e68c2c35633b2b
Phillip khoza <phillipa.khoza@itthynk.co.za>
2021-08-15 20:34:40 +0200

    master

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,17 @@ export class Home extends Component {
     render() {
         return (       
           <div className="site-main">   
-            <Header/>            
+            <Header/>
+            <section>
+                <div class="row">
+                    <div class="col-sm-5 img1">
+                        <img className="img-fluid" src="https://via.placeholder.com/547X850/444444.jpg" title="single-img-five" alt="single-img-five" />
+                    </div>
+                    <div class="col-sm-7">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                    </div>
+                </div>
+            </section>        
           </div>
         )
     }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 export class Header extends Component {
     render() {
         return (   
-           <nav class="navbar navbar-expand-lg navbar-light bg-light">
+           <nav class="navbar navbar-expand-lg navbar-light ">
                 <div class="container">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
