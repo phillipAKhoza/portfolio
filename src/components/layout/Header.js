e17fd32c79fbb227562756fa6614b28d84e0c9e0
Phillip khoza <phillipa.khoza@itthynk.co.za>
2021-08-31 21:54:41 +0200

    nav

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 export class Header extends Component {
     render() {
         return (   
-           <nav id="navbar" class="navbar navbar-expand-sm fixed-top">
+           <nav id="navbar" class="navbar navbar-expand-lg fixed-top">
                 <div class="container">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <a class="navbar-brand" href="#"><img src="img/logo.png" className="featured-logo"/></a>
-                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item active">
                                 <a class="nav-link pk-light active" href="#home">Home</a>
