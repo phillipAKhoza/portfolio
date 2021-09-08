072391c458f5568146cd842da39bac514e0875c3
Phillip khoza <phillipa.khoza@itthynk.co.za>
2021-09-08 11:05:25 +0200

    navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,27 +8,27 @@ export class Header extends Component {
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <a className="navbar-brand" href="https://www.facebook.com/phillipanthony.khoza"><img src="img/logo.png" alt="logo" className="featured-logo"/></a>
+                    <a className="navbar-brand" href={process.env.PUBLIC_URL + '/'}><img src="img/logo.png" alt="logo" className="featured-logo"/></a>
                     <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <a className="nav-link pk-light active" href="#home">Home</a>
+                                <a className="nav-link pk-light active" href="#home" data-toggle="collapse" data-target=".navbar-collapse.show">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link pk-light" href="#about">About</a>
+                                <a className="nav-link pk-light" href="#about" data-toggle="collapse" data-target=".navbar-collapse.show">About</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link pk-light" href="#portfolio">Portfolio</a>
+                                <a className="nav-link pk-light" href="#portfolio" data-toggle="collapse" data-target=".navbar-collapse.show">Portfolio</a>
                             </li>
                              <li className="nav-item">
-                                <a className="nav-link pk-light" href="#certification">Certification</a>
+                                <a className="nav-link pk-light" href="#certification" data-toggle="collapse" data-target=".navbar-collapse.show">Certification</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link pk-light" href="#contact">Contact</a>
+                                <a className="nav-link pk-light" href="#contact" data-toggle="collapse" data-target=".navbar-collapse.show">Contact</a>
                             </li>
                         </ul>
                         <div className="form-inline my-2 my-lg-0">
-                         <a className="btn download btn-outline-warning my-2 my-sm-0" type="submit" href="https://www.facebook.com/phillipanthony.khoza">Download CV <i className="fa fa-download featured-icon"></i></a>
+                         <a className="btn download btn-outline-warning my-2 my-sm-0" type="submit" href="https://drive.google.com/file/d/1zZhGd1kO8BlzcjN1wfuHl28z8HsCkidL/view?usp=sharing">Download CV <i className="fa fa-download featured-icon"></i></a>
                         </div>
                     </div>
                 </div>
